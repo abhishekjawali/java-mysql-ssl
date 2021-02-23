c448c334314911124fc7e6f8fa9681ea6b650f4d
Abhishek <abhishekjv@gmail.com>
2021-02-23 18:14:47 +0530

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Sample code for establishing SSL connection to Aurora MySQL DB from Java
+
+## Pre-requisites
+-  JDK 11
+- Java Key Store file (JKS)
+
+
+
+## Steps to execute
+
+### Setup JKS 
+Optional step. JKS file is already created based on RDS CA and included in this example. The following steps were followed to setup the JKS file. 
+
+1. Download RDS CA certificate (rds-ca-2019-root.pem) from https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
+2. Convert the certificate to .der format
+    ```
+    openssl x509 -outform der -in rds-ca-2019-root.pem -out rds-ca-2019-root.der
+    ```
+3. Import the certificate into the keystore. Set the password for keystore and trust the certificate
+    ```
+    keytool -import -alias rds-root -keystore clientkeystore.jks -file rds-ca-2019-root.der                    
+    ```
+
+### Setup application
+
+1. Checkout code
+    ```
+    git checkout https://github.com/abhishekjawali/java-mysql-ssl.git
+    ```
+2.  [Optional] If new keystore is generated, then add the file to classpath
+3.  Update the DB credentials in DemoApplication.java
+4.  Build the application
+    ```
+    .\mvnw.cmd clean install
+    ```
+5. Run the application. 
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private static final String DB_USER = "DB-USER-NAME";
+	private static final String DB_PASSWORD = "DB-PASSWORD";
+	private static final String DB_URL = "jdbc:mysql://DB-URL";
+	// This key store has only the prod root ca.
+	private static final String KEY_STORE_FILE_PATH = "classpath:clientkeystore.jks";
+	private static final String KEY_STORE_PASS = "password";
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+
+		System.setProperty("javax.net.ssl.trustStore", KEY_STORE_FILE_PATH);
+		System.setProperty("javax.net.ssl.trustStorePassword", KEY_STORE_PASS);
+
+		Properties properties = new Properties();
+		properties.setProperty("sslMode", "Verify_CA");
+		properties.put("user", DB_USER);
+		properties.put("password", DB_PASSWORD);
+
+		Connection connection;
+		try {
+			connection = DriverManager.getConnection(
+				DB_URL, properties);
+					String connClientInfo = connection.getClientInfo().toString();
+					System.out.println("Connection client info "+ connClientInfo);
+					Statement stmt=connection.createStatement();
+	
+		ResultSet rs=stmt.executeQuery("SELECT 1 from dual");
+		rs.next();
+		String res = rs.getString(1);
+		System.out.println("the result is "+res);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(0);
+	}
+
+}
